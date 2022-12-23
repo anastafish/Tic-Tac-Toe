1d1b96fa1c53c5c89ff4a54850fa1d4e2f183683
anastafish <anastafish24@gmail.com>
2022-12-24 00:51:57 +0300

    finished the main Play function and made the main Ui to Enter the players names just a little to complete the project

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const boxArray = []; 
 const box = document.querySelectorAll('.box')
 
-const gameFactor = (mark,turn) => {
+const gameFactor = (name,mark,turn) => {
+    const getName = name;
     const getMark =  mark;
     const getTurn =  turn;
     
-    const gameBoard = () => {
-        return {Board:[
+    const gameBoard =
+        {Board:[
             "","","",
             "","","",
             "","",""]}        
-    }
+    
+
     const render = (() => {        
         const content = () => {for (let i = 0; i < box.length; i++){
             boxArray.push(box[i])
@@ -24,7 +26,7 @@ const gameFactor = (mark,turn) => {
     return{content}    
     })(); 
 
-    const Play = (boxes) => {
+    const Play = (boxes, player1, player2, Board) => {
         for (let i = 0; i < box.length; i++){
             boxes[i].addEventListener('click', () => {
                 if (player1.getTurn === true && boxes[i].textContent === ""){
@@ -33,6 +35,16 @@ const gameFactor = (mark,turn) => {
                     gameFactor().render.content();
                     player1.getTurn = false;
                     player2.getTurn = true;
+                    if(checkWin(Board.Board)) {Board.Board = [
+                        "","","",
+                        "","","",
+                        "","",""]
+                        document.querySelector('.msg').textContent = `${player1.getName} Won!`
+                        document.querySelector('.winmsg').style.display = 'flex'
+                        document.querySelector('.winmsg').style.background = 'red'
+
+                    } 
+
                 } 
                 else if (player2.getTurn === true && boxes[i].textContent === "") {
                     Board.Board[i] = player2.getMark;
@@ -40,23 +52,79 @@ const gameFactor = (mark,turn) => {
                     gameFactor().render.content()
                     player2.getTurn = false;
                     player1.getTurn = true;
+                    if(checkWin(Board.Board)) {Board.Board = [
+                        "","","",
+                        "","","",
+                        "","",""]
+                        document.querySelector('.msg').textContent = `${player2.getName} Won!`;
+                        document.querySelector('.winmsg').style.display = 'flex';
+                        document.querySelector('.winmsg').style.background = 'green'
+
+                    } 
+
                 }
             })
         }  
+                    document.querySelector('.reset').addEventListener('click', () => {
+                        gameFactor().render.content()
+                        document.querySelector('.winmsg').style.display = 'none';
+                        for (let i = 0; i < box.length; i++){
+                            box[i].classList.remove("o", "x")
+                        }
+                    })
     }  
 
-    return{getMark, getTurn, gameBoard, render, Play};
-}
+    const checkWin = (markArray) => {
+        for (let i = 0; i < 9; i += 3) {
+            if (markArray[i], markArray[i+1], markArray[i+2] != ""){
+            if (markArray[i] === markArray[i+1] && markArray[i+1] === markArray[i+2]) {
+                return true;
+            }}
+        }
+        for (let i = 0; i < 4; i += 1) {
+            if (markArray[i], markArray[i+3], markArray[i+6] != ""){
+            if (markArray[i] === markArray[i+3] && markArray[i+3] === markArray[i+6]) {
+                return true;
+            }}
+        }
 
-const Board = gameFactor().gameBoard();
-    const player1 = gameFactor("x", true);
-    const player2 = gameFactor("o", false); 
-
-gameFactor().render.content()   
-
-gameFactor().Play(boxArray)
 
 
+            if (markArray[0], markArray[4], markArray[8] != "") {
+                if (markArray[0] === markArray[4] && markArray[4] === markArray[8]){
+                    return true;
+                }
+            }
+
+            if (markArray[2], markArray[4], markArray[6] != "") {
+                if (markArray[2] === markArray[4] && markArray[4] === markArray[6]){
+                    return true;
+                }
+            }
+        }
+        
+      
+        
+        return{getName, getMark, getTurn, gameBoard, render, Play};
+
+    }
+
+
+    document.querySelector('#start-btn').addEventListener('click', () => {
+            const playerOne = document.querySelector('#playerone')
+            const playerTwo = document.querySelector('#playertwo')
+            const player1 = gameFactor(playerOne.value, "X", true)
+            const player2 = gameFactor(playerTwo.value, "O", false)  
+            gameFactor().render.content() 
+            gameFactor().Play(boxArray, player1, player2, Board)
+            document.querySelector('.container').style.display = "grid";
+            document.querySelector('.start-screen').style.display = "none";
+
+    })
+
+    const Board = gameFactor().gameBoard;
+
+    
   
 
 
